07b7d04e56a617633f8035d0427293e1fb921712
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-05-02 21:13:23 -0300

    Crud - Parte 03

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -13,25 +14,39 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
-@Entity
-@Table(name = "tb_postagens")
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity // Entidade = tabela banco de dados
+@Table(name = "tb_postagens") 
 
 public class Postagem {
 	
-	@Id //primary key
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //autoincrement
+	@Id // Primary key - chave primaria da tabela
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // GeneratedValue = chave sera gerada automaticamente pelo banco de dados, strategy = como PK sera gerada, GenerationType.IDENTITY = autoincrement
  	private Long id;
 	
-	@NotBlank(message = "O atributo titulo e obrigatorio!")
+	@NotBlank(message = "O atributo titulo é obrigatório!") // Atributo nao pode ser nulo e nao pode ser em branco
 	@Size(min = 5, max = 100, message = "O atributo titulo deve conter no minimo 5 e no maximo 100 caracteres!") 
 	private String titulo ;
 	
-	@NotNull(message = "O atributo texto e obrigatorio!")
+	@NotNull(message = "O atributo texto é obrigatório!") // Atributo nao pode ser nulo
 	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no minimo 10 e no maximo 1000 caracteres!") 
 	private String texto;
 	
-	@UpdateTimestamp
+	@UpdateTimestamp // Ira receber data e hora do sistema
 	private LocalDateTime data;
+	
+	@ManyToOne // Classe Postagem tera um relacionamento muitos para um com a classe Tema
+	@JsonIgnoreProperties("postagem") // Quebra a recursividade (loop) durante a execucao JSON
+	private Tema tema; // Tambem criar gets and sets
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 	public Long getId() {
 		return id;
